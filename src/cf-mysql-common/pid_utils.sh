8a08113067b38fb0823dd2473ce935142c78b144
Willcock, Stuart <stuartjjwillcock@gmail.com>
2019-12-11 10:26:55 +0000

    get broker to work

--- src/cf-mysql-common/pid_utils.sh
+++ src/cf-mysql-common/pid_utils.sh
@@ -3,6 +3,36 @@ function pid_is_running() {
   ps -p "${pid}" >/dev/null 2>&1
 }
 
+# pid_guard
+#
+# @param pidfile
+# @param name [String] an arbitrary name that might show up in STDOUT on errors
+#
+# Run this before attempting to start new processes that may use the same :pidfile:.
+# If an old process is running on the pid found in the :pidfile:, exit 1. Otherwise,
+# remove the stale :pidfile: if it exists.
+#
+function pid_guard() {
+  declare pidfile="$1" name="$2"
+
+  echo "------------ STARTING $(basename "$0") at $(date) --------------" | tee /dev/stderr
+
+  if [ ! -f "${pidfile}" ]; then
+    return 0
+  fi
+
+  local pid
+  pid=$(head -1 "${pidfile}")
+
+  if pid_is_running "${pid}"; then
+    echo "${name} is already running, please stop it first"
+    exit 1
+  fi
+
+  echo "Removing stale pidfile"
+  rm "${pidfile}"
+}
+
 # wait_pid_death
 #
 # @param pid
